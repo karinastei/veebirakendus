4c5d7915a7933dfac814bfc72a1ff3005c13f7fe
Karina <karina.steinberg@voco.ee>
2023-03-27 00:22:08 +0300

    Elering päringu modifitseerimine

--- controllers/nord-pool-price.ts
+++ controllers/nord-pool-price.ts
@@ -4,11 +4,25 @@ import axios from "axios";
 const router: Router = Router();
 
 router.get("/nord-pool-price", async (req: Request, res: Response) => {
-    const response = await axios.get(
-      "https://dashboard.elering.ee/api/nps/price"
-    );
-    res.json(response.data);
-  });
-
+  const response = await axios.get(
+    "https://dashboard.elering.ee/api/nps/price?start="+req.query.start+"&end="+req.query.end
+  );
+  switch(req.params.country) {
+    case "ee":
+      res.json(response.data.data.ee);
+      break;
+    case "lv":
+      res.json(response.data.data.lv);
+      break;
+    case "lt":
+      res.json(response.data.data.lt);
+      break;
+    case "fi":
+      res.json(response.data.data.fi);
+      break;
+    default:
+      res.json([]);
+  }
+});
 
 export default router;
